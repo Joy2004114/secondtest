4c4d37c580ba4aa89dbad92121de5f3ac24e139a
RifatHossaiN47 <rifat8851@gmail.com>
2024-11-26 01:25:49 +0600

    add new feature

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const express= require("express");
-const app =express();
+const express = require("express");
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.json([
-        {
-            id:1,
-            name:"joy",
-            age:23
-        },
-        {
-            id:3,
-            name:"joyeee",
-            age:23
-        },
-        {
-            id:4,
-            name:"joydas",
-            age:26
-        },
-        {
-            id:2,
-            name:"e",
-            age:240
-        },
-    ])
+app.get("/", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "rifat",
+      age: 23,
+    },
+    {
+      id: 3,
+      name: "rifateee",
+      age: 23,
+    },
+    {
+      id: 4,
+      name: "jrifatdas",
+      age: 26,
+    },
+    {
+      id: 2,
+      name: "rifat",
+      age: 240,
+    },
+  ]);
 });
-app.listen(5000,()=>{
-    console.log("the port is running at 5000")
-})
+app.listen(5000, () => {
+  console.log("the port is running at sal's ");
+});
